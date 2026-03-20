ed38060e929ac77986049183adca4f5de44093f6
Mondr Abulgasm Ammar Gnedi <146215441+Mondr23@users.noreply.github.com>
2026-03-20 05:03:24 +0000

    Update Database.py

--- app/database.py
+++ app/database.py
@@ -11,7 +11,16 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine( DATABASE_URL)
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set!")
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
